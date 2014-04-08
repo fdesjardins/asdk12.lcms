2c32a3321e14705a01852fed765bff4a34fff648
Forrest Desjardins <desjardinsfg@gmail.com>
2014-04-08 12:40:07 -0400

    Added __init__.py and tox tests

---
+++ lcms/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""LCMS"""
+
+__author__ = 'Forrest Desjardins'
+__copyright__ = 'Copyright (C) 2014, Anchorage School District'
+__license__ = 'BSD'
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import lcms
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import tox
+        err = tox.cmdline(self.test_args)
+        sys.exit(err)
+
+
+setup(
+    name = 'lcms',
+    version = lcms.__version__,
+    license = 'BSD',
+    description = 'Learning Content Management System',
+    packages = find_packages(),
+    classifiers = [
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires = [],
+    include_package_data = True,
+    zip_safe = False,
+    tests_require = ['tox'],
+    cmdclass = {'test': Tox},
+)
